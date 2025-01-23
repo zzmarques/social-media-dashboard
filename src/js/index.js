268247645ca4c250b77c642e2c379155947967b6
zzmarques <walacemarqs@gmail.com>
2025-01-23 13:28:18 -0300

    Add lógica de validação da animação do botão de tema

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,20 @@
 (() => {
     const btnDark = document.querySelector('div.dark-mode-btn');
-    const main = document.querySelector('main')
+    const main = document.querySelector('main');
+    let btnToggle = false
     
     btnDark.addEventListener('click', () => {
-        main.classList.toggle('dark-theme')
+        main.classList.toggle('dark-theme');
+        btnToggle = !btnToggle;
+
+        if (btnToggle) {
+            btnDark.classList.add('animation-right');
+            btnDark.classList.remove('animation-left');
+        } else {
+            btnDark.classList.remove('animation-right');
+            btnDark.classList.add('animation-left');
+            
+        }
     })
     
 })();
